540b6096b0e04d25c7ee36f78a86e86cf75cfc8b
Jens Nolte <git@queezle.net>
2023-07-20 21:42:51 +0200

    Set up lua lsp to check neovim config

--- init.lua
+++ init.lua
@@ -505,10 +505,29 @@ end
 nvim_lsp.lua_ls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
+  -- TODO current settings are for neovim development. Autodetection would be
+  -- required for other projects.
   settings = {
+    Lua = {
+      runtime = {
+        -- Neovim _usually_ runs LuaJIT (run ':lua =jit.version' to check)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
+      },
+    }
   }
 }
-nvim_lsp.rust_analyzer.setup  {
+nvim_lsp.rust_analyzer.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   settings = {
